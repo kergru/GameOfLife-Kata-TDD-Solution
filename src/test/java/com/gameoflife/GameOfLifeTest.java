f86c474ab96292032564688b4726b54426906526
kergru <kersten@freenet.de>
2020-02-05 15:44:45 +0100

    validate nextGen result

--- src/test/java/com/gameoflife/GameOfLifeTest.java
+++ src/test/java/com/gameoflife/GameOfLifeTest.java
@@ -2,7 +2,10 @@ package com.gameoflife;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -20,16 +23,33 @@ class GameOfLifeTest {
 
     @Test
     void nextGen_should_create_new_world() {
-        boolean board[][] = new boolean[1][1];
+        boolean board[][] = new boolean[3][3];
+        board[0][0] = false;
+        board[0][1] = true;
+        board[0][2] = false;
+        board[1][0] = false;
+        board[1][1] = true;
+        board[1][2] = false;
+        board[2][0] = false;
+        board[2][1] = true;
+        board[2][2] = false;
         GameOfLife gol = new GameOfLife(board);
         World world = gol.getWorld();
 
         gol.nextGen();
         World newWorld = gol.getWorld();
 
-        assertNotNull(world);
-        assertNotNull(newWorld);
-        assertNotEquals(world, newWorld);
-        assertTrue(world.getCells().size() == newWorld.getCells().size());
+        assertWorld(newWorld);
+    }
+
+    private void assertWorld(World newWorld) {
+        List<Coordinates> coordsOfLivingCells = newWorld.getCells().stream()
+                .filter(c -> c.isAlive())
+                .map(c -> c.getCoordinates())
+                .collect(toList());
+        assertTrue(coordsOfLivingCells.size() == 3);
+        assertEquals(coordsOfLivingCells.get(0), new Coordinates(1, 0));
+        assertEquals(coordsOfLivingCells.get(1), new Coordinates(1, 1));
+        assertEquals(coordsOfLivingCells.get(2), new Coordinates(1, 2));
     }
 }
