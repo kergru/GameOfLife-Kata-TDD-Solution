fb707ff30a317e75799232b40947b0461079f225
kergru <kersten@freenet.de>
2020-02-05 14:33:31 +0100

    refactor cells list to cells map

--- src/main/java/com/gameoflife/World.java
+++ src/main/java/com/gameoflife/World.java
@@ -1,25 +1,27 @@
 package com.gameoflife;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class World {
 
-    private List<Cell> cells;
+    private Map<Coordinates, Cell> cellMap;
 
     public World(boolean[][] board) {
-        cells = new ArrayList<>();
+        cellMap = new LinkedHashMap<>();
         for (int row = 0; row < board.length; row++) {
             for (int col = 0; col < board[row].length; col++) {
                 boolean alive = board[row][col];
                 Coordinates coordinates = new Coordinates(row, col);
                 Cell cell = new Cell(coordinates, alive);
-                cells.add(cell);
+                cellMap.put(coordinates, cell);
             }
         }
     }
 
     public List<Cell> getCells() {
-        return cells;
+        return new ArrayList<>(cellMap.values());
     }
 }
